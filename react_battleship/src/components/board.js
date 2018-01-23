1ace3fe844cd8f3e69abd7640ccaf849e20c269f
Kevin Truong <kevintru0ng@yahooo.com>
2018-01-23 11:54:37 -0800

    added color for hit/miss

--- react_battleship/src/components/board.js
+++ react_battleship/src/components/board.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+const EMPTY = 0;
+const SHIP = 1;
+const SHIP_HIT = "HIT";
+const EMPTY_HIT = "MISS";
+
 class Board extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: []
+      board: [],
+      bgColor: 'lightblue',
     }
     this.setUpBoard()
   }
@@ -16,22 +22,32 @@ class Board extends Component {
   }
 
   setUpBoard() {
-    for (let i = 0; i < 10; i++) {
-      this.state.board.push([])
+    let board = []
+
+    for(let row = 0; row < 10; row++) {
+      board.push([])
+      for(let col = 0; col < 10; col++) {
+        board[row][col] = EMPTY
+      }
     }
+
+    console.log(board);
+
+    this.state.board = board
   }
 
   placeShip() {
-    let ship = 1;
+
     let i = 0;
     // get the array out of the state
     let newBoard = this.state.board
 
     while(i < 1){
-      let randPosX = Math.floor(Math.random() * 10)
-      let randPosY = Math.floor(Math.random() * 10)
+      let row = Math.floor(Math.random() * 10)
+      let col = Math.floor(Math.random() * 10)
       // update it
-      if(!newBoard[randPosX][randPosY] || newBoard[randPosX][randPosY].length === 0){ newBoard[randPosX][randPosY] = ship
+      if(!newBoard[row][col] || newBoard[row][col].length === 0) {
+        newBoard[row][col] = SHIP
         i++
       }
     }
@@ -41,28 +57,37 @@ class Board extends Component {
     })
   }
 
-  clickHandler(x, y) {
-    let newBoard = this.state.board
-    newBoard[y][x] = 6;
-    this.setState({
-      board: newBoard
-    })
+  clickHandler(row, col, e) {
+    const board = this.state.board
+
+    if (board[col][row] === SHIP) {
+      board[col][row] = SHIP_HIT
+      e.target.style.backgroundColor = 'green'
+    } else if (board[col][row] === EMPTY) {
+      board[col][row] = EMPTY_HIT
+      e.target.style.backgroundColor = 'red'
+    }
+    this.setState({board})
   }
 
-  renderRow(y) {
-    const row = [];
-    for (let x = 0; x < 10; x++) {
-      let theXY = x + "_" + y
-      row.push(
+  renderRow(row) {
+    const { board } = this.state
+
+    const set = [];
+    for (let col = 0; col < 10; col++) {
+      let theXY = col + "_" + row
+      set.push(
         <td id={theXY}
             key={theXY}
-            onClick={this.clickHandler.bind(this,x, y)}
+            onClick={this.clickHandler.bind(this,col, row)}
+            style={{backgroundColor:this.state.bgColor}}
+
         >
-          {this.state.board[y][x]}
+          {board[row][col] !== 0 ? board[row][col] : ''}
         </td>
       );
     }
-    return row;
+    return set;
   }
 
   renderRows() {

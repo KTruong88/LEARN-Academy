2a4e582c0b03e3f19d820c0313b4f0e7a1666060
Kevin Truong <kevintru0ng@yahooo.com>
2018-01-30 14:26:03 -0800

    finished routes/tested w/ postman

--- wiki_fullstack/app.js
+++ wiki_fullstack/app.js
@@ -16,36 +16,36 @@ app.use(cors());
 app.get('/', function (req, res) {
   res.json({message: 'API Example'})
 })
-
+// List of pages
 app.get('/pages', function (req, res) {
     Page.findAll().then( (pages) => {
         res.json({pages:pages})
     })
 })
-
+// Single page
 app.get('/pages/:id', function (req, res) {
     Page.findAll().then( (pages) => {
         res.json({pages:pages})
     })
 })
-
+// Create page
 app.post('/pages', (req, res) => {
 
-  // We setup a validation check for 'name'
+  // We setup a validation check for 'name, subject, content'
   req.checkBody('name', 'Is required').notEmpty()
   req.checkBody('subject', 'Is required').notEmpty()
   req.checkBody('content', 'Is required').notEmpty()
   // Now we can run our validations
   req.getValidationResult()
-    .then((validationErrors) =>{
+    .then((validationErrors) => {
 
-      // If there are no errors, go ahead and create cat
-      if(validationErrors.isEmpty()){
+      // If there are no errors, go ahead and create page
+      if(validationErrors.isEmpty()) {
         Page.create({
           name: req.body.name,
           subject: req.body.subject,
           content: req.body.content
-        }).then((page)=>{
+        }).then((page) => {
           res.status(201)
           res.json({page: page})
         })
@@ -57,7 +57,7 @@ app.post('/pages', (req, res) => {
     }
   })
 })
-
+// Update page
 app.put('/pages/:id', (req, res) => {
     // all params come in as strings
 
@@ -66,7 +66,21 @@ app.put('/pages/:id', (req, res) => {
         subject: req.body.subject,
         content: req.body.content
     },{
-        where: { id: parseInt(req.params.id) }
+        where: {
+            id: parseInt(req.params.id)
+        }
+    }).then(function (result) {
+        res.json(result)
+    })
+})
+// Delete page
+app.delete('/pages/:id', (req, res) => {
+    Page.destroy({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    }).then(function (result) {
+        res.json(result)
     })
 })
 
